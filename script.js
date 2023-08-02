6902ed71a876606f67ad7706d5f2437d23cd07cc
Jayakodehode <jaya.kodehode@gmail.com>
2023-08-02 12:55:07 +0200

    done with media queries

---
+++ script.js
@@ -0,0 +1,6 @@
+const menu = document.getElementById("menubar");
+const navbar = document.querySelector(".navbar");
+menu.addEventListener("click", () => {
+  console.log("cliked");
+  navbar.classList.toggle("active");
+});
